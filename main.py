41f92228cbe6a5f447b8bf3a4fbb0d947985b7b7
ma173 <malte.gerhardt@rub.de>
2020-10-10 16:16:23 +0000

    Suche mit Regex in neuem Lernalgorithmus eingebaut

--- main.py
+++ main.py
@@ -283,12 +283,16 @@ def learningAlgorithmAnnotatedTexts():
         fileContents=f.read()
         f.close()
       dataLines = fileContents.splitlines()
+      featureList=[]
       for dataLine in dataLines:
         if len(dataLine)>1:
           dataEntries = dataLine.split("\t")
           typeOfData = dataEntries[1].split(" ")[0]
           actualData = dataEntries[2]
           print("Type of data: {} -   Actual data: {}".format(typeOfData,actualData))
+          searchPattern = '\b(\S+)({})(\S+)\b'.format(actualData)
+          searchResult = re.findall(searchPattern,websiteData)
+          print(searchResult)
           #TODO: Herausfinden, mit welchem Tool Frederic empfahl, in HTML-Code nach Inhalten zu suchen, um den Tag/ das Feature dazu zu finden (um eben diese "actualData" auf der geholten "websiteData" zu finden und auf Tag/ Feature zu schließen. War das BeautifulSoup wie in tryout.py?)
     elif filename.endswith(".txt"):
       websiteStyle="websitetext"
