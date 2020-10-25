edfb97999e2d19ab53bca4f4ea94d632bb5900e3
ma173 <malte.gerhardt@rub.de>
2020-10-25 22:28:52 +0000

    Ersetzung von Zeichen in Regex-Code angepasst

--- main.py
+++ main.py
@@ -289,8 +289,10 @@ def learningAlgorithmAnnotatedTexts():
           dataEntries = dataLine.split("\t")
           typeOfData = dataEntries[1].split(" ")[0]
           actualData = dataEntries[2]
-          if "+" in actualData:
-            actualData=actualData.replace("+","\+")
+          charsToReplace=["+","(",")"]
+          for character in actualData:
+            if character in charsToReplace:
+              actualData=actualData.replace(character,"\\"+character)
           print("Type of data: {} -   Actual data: {}".format(typeOfData,actualData))
           searchPattern = '\b(\S+)({})(\S+)\b'.format(actualData)
           print("Search pattern is: {}".format(searchPattern))
